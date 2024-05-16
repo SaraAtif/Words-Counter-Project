b7ff1c387f941dbbb98ceff8844a9da4f53697e9
SaraAtif <93081788+SaraAtif@users.noreply.github.com>
2024-05-16 22:13:38 +0500

    words counter

---
+++ main.js
@@ -0,0 +1,17 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.magentaBright.bold("\n \t\t Wellcome to Sara - words counter \n"));
+console.log("~".repeat(60));
+let answer = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence: "
+    }
+]);
+let words = answer.sentence.trim().split(" ");
+console.log("~".repeat(60));
+console.log(chalk.bold("\n- Sentence words: \n"));
+console.log(words);
+console.log(chalk.bold(`\n Total words: ${chalk.magenta.bold(words.length)}`));
+console.log("~".repeat(60));
